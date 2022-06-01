10911ff8137813986e4788c49919c6ed382b72b8
Martin <nitromagix@gmail.com>
2022-05-31 20:48:30 -0700

    added edit and update routes

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -22,42 +22,67 @@ breads.get('/new', (req, res) => {
    res.render('new')
 });
 
-   //    trace('/breads/')(params);
+//    trace('/breads/')(params);
+
+
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+   res.render('edit', {
+     bread: Bread[req.params.indexArray],
+     index: req.params.indexArray
+   })
+})
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
    if (Bread[req.params.arrayIndex]) {
-     res.render('show', {
-       bread:Bread[req.params.arrayIndex],
-       index: req.params.arrayIndex,
-     })
+      res.render('show', {
+         bread: Bread[req.params.arrayIndex],
+         index: req.params.arrayIndex,
+      })
    } else {
-     res.render('404')
+      res.render('show')
    }
- })
- 
+})
 
-// CREATE
+breads.get('*', (req, res) => {
+
+   res.render('error404')
+
+})
 
 // CREATE
 breads.post('/', (req, res) => {
    if (!req.body.image) {
-     req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+      req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
    }
-   if(req.body.hasGluten === 'on') {
-     req.body.hasGluten = true
+   if (req.body.hasGluten === 'on') {
+      req.body.hasGluten = true
    } else {
-     req.body.hasGluten = false
+      req.body.hasGluten = false
    }
    Bread.push(req.body)
    res.redirect('/breads')
- })
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+   if (req.body.hasGluten === 'on') {
+      req.body.hasGluten = true
+   } else {
+      req.body.hasGluten = false
+   }
+   Bread[req.params.arrayIndex] = req.body
+   res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
 
 // DELETE
 breads.delete('/:indexArray', (req, res) => {
    Bread.splice(req.params.indexArray, 1)
    res.status(303).redirect('/breads')
- })
- 
+})
+
+
 
 module.exports = breads
